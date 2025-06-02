bab46f02c175eb71b79e476ffc071e665bb3c428
Amir <najiamirdesktop@gmail.com>
2025-06-02 16:52:54 +0330

    add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import NavBar from "./components/app/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
---
+++ src/components/app/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <nav className="flex justify-between h-16 shadow-sm shadow-gray-50 px-8 py-4">
+      <h1 className="text-2xl font-semibold">پنل مدیریت کارپرداز</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
