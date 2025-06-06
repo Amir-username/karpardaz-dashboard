87695f4623118e1367827a90cc4a75a7b1352995
Amir <najiamirdesktop@gmail.com>
2025-06-06 15:47:17 +0330

    add employer detail & ad requests table

--- src/pages/Employer/EmployerDetailsPage/EmployerDetailsPage.tsx
+++ src/pages/Employer/EmployerDetailsPage/EmployerDetailsPage.tsx
@@ -1,7 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Table, TableCell, TableRow } from "@/components/ui/table";
+import { fetchEmployerDetail } from "@/fetch/employer/fetchEmployerDetail";
+import type { EmployerDetail } from "@/models/EmployerDetail";
+import { useState } from "react";
+
 function EmployerDetailsPage() {
-    return (
-        <div>Employer Details Page</div>
-    );
+  const [searchInput, setSearchInput] = useState("");
+  const [employerDetail, setEmployerDetail] = useState<EmployerDetail>();
+
+  const handleSearchDetails = () => {
+    const fetchDetails = async () => {
+      const data = await fetchEmployerDetail(Number(searchInput));
+      setEmployerDetail(data);
+    };
+
+    fetchDetails();
+    setSearchInput("");
+  };
+
+  return (
+    <div className="flex flex-col gap-10 items-center justify-center w-full h-full m-32 ">
+      <h1 className="text-2xl">لیست جزئیات کارفرما</h1>
+      <div className="flex gap-1 w-96">
+        <Button onClick={handleSearchDetails}>جستجو</Button>
+        <Input
+          type="numbers"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="شناسه کارفرما"
+        />
+      </div>
+      {employerDetail && (
+        <div className="rounded-lg shadow-sm ring-1 ring-gray-200">
+          <Table className="p-2">
+            <TableRow className="flex w-full font-bold p-2">
+              <TableCell className="w-64">شناسه</TableCell>
+              <TableCell className="w-64">نام سازمان</TableCell>
+              <TableCell className="w-64">وبسایت</TableCell>
+              <TableCell className="w-64">جمعیت</TableCell>
+            </TableRow>
+            <TableRow className="flex w-full p-2">
+              <TableCell className="w-64">{employerDetail.id}</TableCell>
+              <TableCell className="w-64">
+                {employerDetail.company_name}
+              </TableCell>
+              <TableCell className="w-64">{employerDetail.website}</TableCell>
+              <TableCell className="w-64">
+                {employerDetail.population}
+              </TableCell>
+            </TableRow>
+          </Table>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default EmployerDetailsPage;
--- src/pages/Employer/EmployerRequestsPage/EmployerRequestsPage.tsx
+++ src/pages/Employer/EmployerRequestsPage/EmployerRequestsPage.tsx
@@ -1,7 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Table, TableCell, TableRow } from "@/components/ui/table";
+import { BASE_LINK } from "@/config";
+import type { AdRequestModel } from "@/models/AdRequest";
+import { useState } from "react";
+
 function EmployerRequestsPage() {
-    return (
-        <div>Employer requests page</div>
-    );
+  const [searchInput, setSearchInput] = useState("");
+  const [requests, setRequests] = useState<AdRequestModel[]>([]);
+
+  const handleSearchRequests = () => {
+    const fetchRequests = async () => {
+      const res = await fetch(
+        BASE_LINK + `advertise-requests/${Number(searchInput)}`
+      );
+      const data = await res.json();
+
+      if (res.status === 200) setRequests(data);
+    };
+
+    fetchRequests();
+    setSearchInput("");
+  };
+
+  return (
+    <div className="flex flex-col gap-10 items-center justify-center w-full h-full m-32 ">
+      <h1 className="text-2xl">لیست درخواست های آگهی کارفرما</h1>
+      <div className="flex gap-1 w-96">
+        <Button onClick={handleSearchRequests}>جستجو</Button>
+        <Input
+          type="numbers"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="شناسه آگهی"
+        />
+      </div>
+      {requests.length > 0 && (
+        <div className="rounded-lg shadow-sm ring-1 ring-gray-200">
+          <Table className="p-2">
+            <TableRow className="flex w-full font-bold p-2">
+              <TableCell className="w-64">شناسه</TableCell>
+              <TableCell className="w-64">شناسه آگهی</TableCell>
+              <TableCell className="w-64">شناسه کارجو</TableCell>
+              <TableCell className="w-64">وضعیت درخواست</TableCell>
+            </TableRow>
+            {requests.map((req) => (
+              <TableRow className="flex w-full p-2" key={req.id}>
+                <TableCell className="w-64">{req.id}</TableCell>
+                <TableCell className="w-64">{req.advertise_id}</TableCell>
+                <TableCell className="w-64">{req.jobseeker_id}</TableCell>
+                <TableCell className="w-64">{req.status}</TableCell>
+              </TableRow>
+            ))}
+          </Table>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default EmployerRequestsPage;
