7abd5906f0c371c389119ccb01ba21d93c498193
amir <najiamirdesktop@gmail.com>
2025-08-18 21:37:20 +0330

    update base link

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const BASE_LINK = 'http://127.0.0.1:8000/'
+// export const BASE_LINK = 'http://127.0.0.1:8000/'
+
+export const BASE_LINK = 'https://underlying-umeko-univercityproject317-ea179cc6.koyeb.app/'
